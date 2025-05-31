3566a6fd110d45013fb21aa6f7928c06e8dc0498
ridam3502 <ridamsharma3502@gmail.com>
2025-05-31 11:33:03 +0530

    add

---
+++ experiment/pretest.js
@@ -0,0 +1,52 @@
+var myQuestions = [
+  {
+    question: "What does the 'B' parameter in ABCD parameters physically represent in a transmission line?",
+    answers: {{
+      a: "Shunt admittance",
+      b: "Series impedance",
+      c: "Sending-end voltage",
+      d: "Power factor"
+    }},
+    correctAnswer: "b"
+  },
+  {
+    question: "Which ABCD model is best suited for long transmission lines?",
+    answers: {{
+      a: "Short line model",
+      b: "Nominal T model",
+      c: "Nominal π model",
+      d: "Long line model"
+    }},
+    correctAnswer: "d"
+  },
+  {
+    question: "In ABCD parameters, which relation holds true for a lossless transmission line?",
+    answers: {{
+      a: "AD - BC = 0",
+      b: "AD - BC = 1",
+      c: "A + D = 0",
+      d: "A = D = 0"
+    }},
+    correctAnswer: "b"
+  },
+  {
+    question: "What is the role of ABCD parameters in power system analysis?",
+    answers: {{
+      a: "Calculate power losses",
+      b: "Determine cable size",
+      c: "Relate sending and receiving end voltages and currents",
+      d: "Control voltage regulation only"
+    }},
+    correctAnswer: "c"
+  },
+  {
+    question: "In the ABCD parameter matrix, which element corresponds to the shunt admittance effect?",
+    answers: {{
+      a: "A",
+      b: "B",
+      c: "C",
+      d: "D"
+    }},
+    correctAnswer: "c"
+  }
+];
